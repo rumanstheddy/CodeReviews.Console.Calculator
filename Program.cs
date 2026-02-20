4823596415cbf66ee3eded7ca8ec74e12eb4f53d
Sumanth Reddy <sumanthcob@gmail.com>
2026-02-20 14:20:24 -0500

    refactor: streamline user input for result selection and storage in Calculator

--- Program.cs
+++ Program.cs
@@ -24,29 +24,31 @@ namespace CalculatorProgram
 
                 if (calculator.ResultHistory.Count > 0)
                 {
-                    Console.Write("Would you like to use a previously stored result? \n");
-                    Console.WriteLine("\ty - Yes");
-                    Console.WriteLine("\tn - No");
-                    string? decision = Console.ReadLine();
+                    // Console.Write("Would you like to use a previously stored result? \n");
+                    // Console.WriteLine("\ty - Yes");
+                    // Console.WriteLine("\tn - No");
+                    // string? decision = Console.ReadLine();
+                    // Console.WriteLine();
 
-                    while (decision == null || !Regex.IsMatch(decision, "[y | n]"))
-                    {
-                        Console.WriteLine("Error: Unrecognized input. Please try again.");
-                    }
-
-                    if (decision == "y")
+                    Console.Write("Press 'p' to use a previously stored result, or press any other key and Enter to continue: ");
+                    if (Console.ReadLine() == "p")
                     {
                         Console.WriteLine("Select which results you would like to use: \n");
+
                         for (int i = 0; i < calculator.ResultHistory.Count; i++)
                         {
                             Console.WriteLine($"\t{i + 1} - {calculator.ResultHistory[i]}");
                         }
+
+                        Console.WriteLine();
+                        Console.Write("Your option? ");
                         string? selectedResult = Console.ReadLine();
                         while (selectedResult == null && int.Parse(selectedResult) > 0 &&
                         int.Parse(selectedResult) <= calculator.ResultHistory.Count)
                         {
                             Console.Write("This is not valid input. Please select an appropriate option.");
                             selectedResult = Console.ReadLine();
+                            // Console.WriteLine();
                         }
 
                         result = calculator.ResultHistory[int.Parse(selectedResult) - 1];
@@ -54,8 +56,10 @@ namespace CalculatorProgram
                 }
 
                 // Ask the user to type the first number.
+                Console.WriteLine();
                 Console.Write("Type a number, and then press Enter: ");
                 numInput1 = Console.ReadLine();
+                Console.WriteLine();
 
                 double cleanNum1 = 0;
                 while (!double.TryParse(numInput1, out cleanNum1))
@@ -80,7 +84,8 @@ namespace CalculatorProgram
                 Console.WriteLine("\ta - Add");
                 Console.WriteLine("\ts - Subtract");
                 Console.WriteLine("\tm - Multiply");
-                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\td - Divide\n");
+
                 Console.Write("Your option? ");
 
                 string? op = Console.ReadLine();
@@ -100,56 +105,98 @@ namespace CalculatorProgram
                     }
                     else
                     {
+                        Console.WriteLine();
                         Console.WriteLine("Your result: {0:0.##}\n", result);
 
-                        if (!calculator.ShouldStoreResults)
+                        // Console.WriteLine("Would you like to store this result?");
+                        // Console.WriteLine("\ty - Yes");
+                        // Console.WriteLine("\tn - No");
+                        // string? storeDecisionSingle = Console.ReadLine();
+                        // while (storeDecisionSingle == null || !Regex.IsMatch(storeDecisionSingle, "[y | n]"))
+                        // {
+                        //     Console.WriteLine("Error: Unrecognized input. Please try again.");
+                        // }
+
+                        // Console.WriteLine("------------------------\n");
+                        // if (storeDecisionSingle == "y")
+                        // {
+                        //     // calculator.ShouldStoreResults = true;
+                        //     Console.WriteLine("Your result has been stored.\n", result);
+                        //     calculator.ResultHistory.Add(result);
+
+                        //     if (!calculator.ShouldStoreResults)
+                        //     {
+                        //         Console.WriteLine("Would you like to keep storing results from now on?");
+                        //         Console.WriteLine("\ty - Yes");
+                        //         Console.WriteLine("\tn - No");
+                        //         string? storeDecision = Console.ReadLine();
+                        //         while (storeDecision == null || !Regex.IsMatch(storeDecision, "[y | n]"))
+                        //         {
+                        //             Console.WriteLine("Error: Unrecognized input. Please try again.");
+                        //         }
+
+                        //         Console.WriteLine("------------------------\n");
+                        //         if (storeDecision == "y")
+                        //         {
+                        //             calculator.ShouldStoreResults = true;
+                        //             Console.WriteLine("Your results will be stored.\n", result);
+                        //             calculator.ResultHistory.Add(result);
+                        //         }
+                        //         // else
+                        //         // {
+                        //         //     Console.WriteLine("Your results was NOT stored.\n", result);
+                        //         // }
+                        //     }
+                        // }
+                        // else
+                        // {
+                        //     Console.WriteLine("Your result was NOT stored.\n", result);
+                        // }
+                        // Wait for the user to respond before closing.
+
+                        Console.WriteLine("------------------------\n");
+                        if (calculator.ResultHistory.Count > 0)
                         {
-                            Console.WriteLine("Would you like to continue storing results from now on?");
-                            Console.WriteLine("\ty - Yes");
-                            Console.WriteLine("\tn - No");
-                            string? storeDecision = Console.ReadLine();
-                            while (storeDecision == null || !Regex.IsMatch(storeDecision, "[y | n]"))
-                            {
-                                Console.WriteLine("Error: Unrecognized input. Please try again.");
-                            }
+                            // Console.WriteLine("Would you like to delete the previously stored results?");
+                            // Console.WriteLine("\ty - Yes");
+                            // Console.WriteLine("\tn - No");
+                            // string? deleteDecision = Console.ReadLine();
+                            // while (deleteDecision == null || !Regex.IsMatch(deleteDecision, "[y | n]"))
+                            // {
+                            //     Console.WriteLine("Error: Unrecognized input. Please try again.");
+                            // }
 
-                            Console.WriteLine("------------------------\n");
-                            if (storeDecision == "y")
-                            {
-                                calculator.ShouldStoreResults = true;
-                                Console.WriteLine("Your result has been stored.\n", result);
-                                calculator.ResultHistory.Add(result);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Your result was not stored.\n", result);
-                            }
-                        }
+                            // Console.WriteLine("------------------------\n");
+                            // if (deleteDecision == "y")
+                            // {
+                            //     calculator.ResultHistory.Clear();
+                            //     calculator.ShouldStoreResults = false;
+                            //     Console.WriteLine("Previous results have been deleted.\n", result);
+                            // }
+                            // else
+                            // {
+                            //     Console.WriteLine("Previous results were NOT deleted.\n", result);
 
-                        else
-                        {
-                            Console.WriteLine("Would you like to delete the previously stored results?");
-                            Console.WriteLine("\ty - Yes");
-                            Console.WriteLine("\tn - No");
-                            string? deleteDecision = Console.ReadLine();
-                            while (deleteDecision == null || !Regex.IsMatch(deleteDecision, "[y | n]"))
-                            {
-                                Console.WriteLine("Error: Unrecognized input. Please try again.");
-                            }
+                            // }
 
-                            Console.WriteLine("------------------------\n");
-                            if (deleteDecision == "y")
+                            Console.Write("Press 'd' to delete all previously stored results, or press any other key and Enter to continue: ");
+                            if (Console.ReadLine() == "d")
                             {
+                                Console.WriteLine();
                                 calculator.ResultHistory.Clear();
-                                // calculator.ShouldStoreResults = false;
-                                Console.WriteLine("Previous results have been deleted.\n", result);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Previous results were not deleted.\n", result);
+                                Console.WriteLine("Previously stored results have been deleted.");
                             }
                         }
 
+                        Console.Write("Press 's' to store this result, or press any other key and Enter to continue: ");
+                        if (Console.ReadLine() == "s")
+                        {
+                            Console.WriteLine();
+                            calculator.ResultHistory.Add(result);
+                            Console.WriteLine("Result stored.");
+                        }
+
+                        Console.WriteLine();
                         Console.WriteLine($"The calculator was used {calculator.UsageCount} time(s).\n");
                         Console.WriteLine("------------------------\n");
                     }
@@ -162,7 +209,7 @@ namespace CalculatorProgram
                 Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
                 if (Console.ReadLine() == "n") endApp = true;
 
-                Console.WriteLine("\n"); // Friendly linespacing.
+                Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------\n");
             }
             calculator.Finish();
             return;
